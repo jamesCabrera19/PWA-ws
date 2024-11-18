5f0f3a7dc73b3293e94338ed77b4757b5c4b4e3f
JamesCabrera <xplayalimbox92@hotmail.com>
2024-11-17 23:20:34 -0600

    added stuff

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,63 @@
+import { View, StyleSheet } from "react-native";
+
+import Button from "../components/Button";
+import ImageViewer from "../components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+const PlaceholderImage = require("@/assets/images/dial.jpg");
+
+export default function Index() {
+    const [image, setImage] = useState<string | undefined>(undefined);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ["images"],
+            allowsEditing: false,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        } else {
+            alert("No image selected");
+        }
+    };
+    const resetImage = () => {
+        setImage(PlaceholderImage);
+    };
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer
+                    imgSource={PlaceholderImage}
+                    selectedImage={image}
+                />
+            </View>
+            <View style={styles.footerContainer}>
+                <Button
+                    label="Choose a photo"
+                    theme="primary"
+                    onPress={pickImageAsync}
+                />
+                <Button label="Reset" onPress={resetImage} />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#25292e",
+        alignItems: "center",
+    },
+    imageContainer: {
+        flex: 1,
+        paddingTop: 28,
+    },
+    footerContainer: {
+        flex: 1 / 2,
+        alignItems: "center",
+    },
+});
